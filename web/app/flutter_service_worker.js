7565239c3ab1ee1b0cb98117e81ca2ed4891d9d3
zemlyanikin max <zemlyanikin.max@yandex.ru>
2020-06-11 13:24:31 +0300

    Update web app.

--- web/app/flutter_service_worker.js
+++ web/app/flutter_service_worker.js
@@ -2,9 +2,9 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "55da2bb81b7726984f165cef7d9a21df",
-"/": "0a9b79e3fed31990ad87e65e1844a47c",
-"app/index.html": "0a9b79e3fed31990ad87e65e1844a47c",
-"app/main.dart.js": "e9fca62da1c84cb91e0c875376c1afc8",
+"/": "55da2bb81b7726984f165cef7d9a21df",
+"app/index.html": "55da2bb81b7726984f165cef7d9a21df",
+"app/main.dart.js": "9929b9c30cfe85cbf097bfbfb39201c9",
 "app/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "app/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "app/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "app/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "app/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "app/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"main.dart.js": "9929b9c30cfe85cbf097bfbfb39201c9",
+"main.dart.js": "91d041525445f4493393344d96dc6709",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
